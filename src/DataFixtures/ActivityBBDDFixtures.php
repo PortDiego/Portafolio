5d578defad3e9e8c4a8aa8f95dcc8a6153d6a82a
Diego Luis <118004337+PortDiego@users.noreply.github.com>
2024-11-28 12:19:27 +0100

    Arreglos migrations (#8)
    * Arreglos Fixtures
    
    * merge

--- src/DataFixtures/ActivityBBDDFixtures.php
+++ src/DataFixtures/ActivityBBDDFixtures.php
@@ -332,7 +332,7 @@ class ActivityBBDDFixtures extends Fixture implements DependentFixtureInterface
     public function getDependencies()
     {
         return [
-            SubcategoryFixture::class,
+            SubcategoryFixtures::class,
             ProvincesFixtures::class,
         ];
     }
